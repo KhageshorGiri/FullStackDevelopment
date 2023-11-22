2b42654f79ced5fe599892707aed901125045991
KhageshorGiri <girikhageshor2432@gmail.com>
2023-11-22 20:39:51 +0545

    basic dto setup

---
+++ SudentMgnt/backend/StudentDemo/Student.Application/Dtos/CourseDto.cs
@@ -0,0 +1,40 @@
+﻿using Student.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student.Application.Dtos
+{
+    public class CourseDto
+    {
+        public Guid CourseId { get; set; }
+
+        [Required(ErrorMessage = "Course Title Cannot be Null or Empty.")]
+        [StringLength(250, ErrorMessage = "Course Title Should be of 250 Characters.")]
+        public string? CourseTitle { get; set; }
+
+        [Required(ErrorMessage = "Course Description Cannot be Null or Empty.")]
+        [StringLength(250, ErrorMessage = "Course Description Should be of 500 Characters.")]
+        public string? CourseDescription { get; set; }
+        
+    }
+
+    public class  CreateCourseDto
+    {
+        [Required(ErrorMessage = "Course Title Cannot be Null or Empty.")]
+        [StringLength(250, ErrorMessage = "Course Title Should be of 250 Characters.")]
+        public string? CourseTitle { get; set; }
+
+        [Required(ErrorMessage = "Course Description Cannot be Null or Empty.")]
+        [StringLength(250, ErrorMessage = "Course Description Should be of 500 Characters.")]
+        public string? CourseDescription { get; set; }
+    }
+
+    public class UpdateCourseDto
+    {
+
+    }
+}
---
+++ SudentMgnt/backend/StudentDemo/Student.Application/Dtos/StudentDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student.Application.Dtos
+{
+    public class StudentDto
+    {
+        public Guid StudentId { get; set; }
+
+        [Required(ErrorMessage = "Student Name Cannot be Null or Empty.")]
+        [StringLength(250, ErrorMessage = "Student Name Should be of 250 Characters.")]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Faculty Name Cannot be Null or Empty.")]
+        [StringLength(250, ErrorMessage = "Faculty Name Should be of 250 Characters.")]
+        public string Faculty { get; set; } = string.Empty;
+    }
+
+    public class CreateStudnetDto
+    {
+        [Required(ErrorMessage = "Student Name Cannot be Null or Empty.")]
+        [StringLength(250, ErrorMessage = "Student Name Should be of 250 Characters.")]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Faculty Name Cannot be Null or Empty.")]
+        [StringLength(250, ErrorMessage = "Faculty Name Should be of 250 Characters.")]
+        public string Faculty { get; set; } = string.Empty;
+    }
+
+    public class UpdateStudentDto
+    {
+
+    }
+}
