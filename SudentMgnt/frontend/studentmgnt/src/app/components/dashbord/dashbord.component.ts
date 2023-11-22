deb2341982694db08c6421c135f6f7b72c755a50
KhageshorGiri <girikhageshor2432@gmail.com>
2023-11-22 21:27:10 +0545

    setting angular project structure

---
+++ SudentMgnt/frontend/studentmgnt/src/app/components/dashbord/dashbord.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dashbord',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dashbord.component.html',
+  styleUrl: './dashbord.component.scss'
+})
+export class DashbordComponent {
+
+}
