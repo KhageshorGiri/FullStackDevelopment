7928d51635609f0ede4c390c3b244e1f217c9af1
KhageshorGiri <girikhageshor2432@gmail.com>
2023-11-23 21:38:40 +0545

    palying with angular componenet

--- SudentMgnt/frontend/studentmgnt/src/app/components/layout/header/header.component.ts
+++ SudentMgnt/frontend/studentmgnt/src/app/components/layout/header/header.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule
+    ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  
+  @Output() toggleSidebarForMe: EventEmitter<any> = new EventEmitter();
 
+  ngOnInit(): void {
+   // ...
+  }
+
+  toggleSidebar() {
+    this.toggleSidebarForMe.emit();
+  }
 }
