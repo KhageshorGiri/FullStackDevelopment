bad8bce9580768cee7197dbc2be2b791774cdae3
KhageshorGiri <girikhageshor2432@gmail.com>
2023-11-24 11:29:42 +0545

    configuring auto mapper

--- SudentMgnt/backend/StudentDemo/Student.API/Program.cs
+++ SudentMgnt/backend/StudentDemo/Student.API/Program.cs
@@ -1,45 +1,60 @@
 using Microsoft.EntityFrameworkCore;
-using Student.Application.AutoMapper;
+using Student.Application;
 using Student.Application.Interfaces.RepositoryInterfaces;
 using Student.Application.Interfaces.ServiceInterfaces;
 using Student.Application.Services;
 using Student.Domin.DataContext;
 using Student.Domin.Repositories;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// adding db configuration
-builder.Services.AddDbContext<StudentDbContext>(options => {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
-});
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-
-// Register Service
-builder.Services.AddScoped<IStudent, StudentService>();
-builder.Services.AddScoped<ICourse, CourseService>();
-builder.Services.AddScoped<IStudentRepository, StudentRepository>();
-builder.Services.AddScoped<ICourseRepository, CourseRepository>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // adding db configuration
+        builder.Services.AddDbContext<StudentDbContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+        });
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddApplication();
+
+        try
+        {
+
+
+            // Register Service
+            builder.Services.AddScoped<IStudent, StudentService>();
+            builder.Services.AddScoped<ICourse, CourseService>();
+            builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+            builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+
+            var app = builder.Build();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+        catch (Exception ex)
+        {
+            string message = ex.Message;
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ SudentMgnt/backend/StudentDemo/Student.Application/ConfigureService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Student.Application
+{
+    public static class ConfigureService
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
