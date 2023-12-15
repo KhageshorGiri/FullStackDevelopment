a05bd62a6ee6cc98aea908320ebf66060090e011
KhageshorGiri <girikhageshor2432@gmail.com>
2023-12-15 21:06:04 +0545

    working in studetails card design

--- SudentMgnt/frontend/studentmgnt/src/app/components/student-detail/student-detail.component.ts
+++ SudentMgnt/frontend/studentmgnt/src/app/components/student-detail/student-detail.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-student-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule,
+     MatCardModule],
   templateUrl: './student-detail.component.html',
   styleUrl: './student-detail.component.scss'
 })
